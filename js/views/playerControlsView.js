605b3232c3c0b6f19e9004d4cd54fa579c0f54b0
Georg Lang <georglang0788@gmx.net>
2013-04-23 07:46:20 +0200

    added style to control buttons

--- js/views/playerControlsView.js
+++ js/views/playerControlsView.js
@@ -2,7 +2,14 @@ define(['jQuery', 'views/playerControlsView'], function($) {
 	
 	var PlayerControlsView = function (model, $element) {
 
-		$element.append('<div id="play">Play</div><div id="pause">Pause</div><div id="stop">Stop</div><div id="next">Next</div>');
+		$element.append('<div class"btn-group>'+ 
+							'<button id="prev"<a class="btn" href="#"><i class="icon-step-backward"></i></a></button>'+
+						 	'<button id="play" <a class="btn" href="#"><i class="icon-play"></i></a></button>' +
+						 	'<button id="pause"<a class="btn" href="#"><i class="icon-pause"></i></a></button>'+
+						 	'<button id="stop"<a class="btn" href="#"><i class="icon-stop"></i></a></button>'+
+						 	'<button id="next" <a class="btn" href="#"><i class="icon-step-forward"></i></a></button>'+
+						 '</div>'
+		);
 
 		
 		$("#play").on("click", function(event){
@@ -21,8 +28,14 @@ define(['jQuery', 'views/playerControlsView'], function($) {
 
 		$("#next").on("click", function(event){
 			console.log("next Song")
-			model.next();
+			model.nextTrack();
 		});
+
+		$("#prev").on("click", function(event){
+			console.log("next Song")
+			model.prevTrack();
+		});
+
 
 		$("#volumeSlider").on("change", function(event){
 			alert("VALUE CHANGED");
